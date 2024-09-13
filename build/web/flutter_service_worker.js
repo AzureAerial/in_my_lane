c6e30b57f98dc541cadfb41a17a332b040ea72a9
Azure Aerial <AzureAerialLLC@gmail.com>
2024-09-13 14:39:02 -0400

    deploy updated web app

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7f0127a1288f94e064d9f951ab4adf4c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "8518e4477df32071ddf24f89057d5410",
+"flutter_bootstrap.js": "070279b1fb6f47c0c6ec132a94af7efe",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
