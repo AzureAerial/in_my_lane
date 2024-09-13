d4f022e325ddf1f5673d0ca0a94414e184e08c84
Azure Aerial <AzureAerialLLC@gmail.com>
2024-09-13 16:14:17 -0400

    Cleaned up project structure

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7f0127a1288f94e064d9f951ab4adf4c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "adc11464d2ee3bee22ff76586dcef7ca",
+"flutter_bootstrap.js": "5e28388b8b12eca5ad53ce3ebafdf118",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
